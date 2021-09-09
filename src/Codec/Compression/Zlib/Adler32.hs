f279577c329e2bb5c4dcbae47217f97163f12733
Adam Wick <awick@galois.com>
2021-09-09 10:55:29 -0700

    Further adjustments to the Adler computation, including a case split on input size.
    Most other implementations of Adler32 use different cases for blocks of
    1 byte, many-but-less-than-16-or-5552 bytes, and lots of bytes. The idea
    for the mid-sized chunks is that you can avoid doing a lot of
    extraneous modulo operations if you batch up the adds in a way that
    doesn't allow the values to roll over. In addition, we bite the bullet
    and switch to unlifted word values to do the computation; this has a
    number of beneficial effects, while retaining safety, including a lack
    of unnecessary type conversions. Finally, shifting from `foldl` to
    `foldl'` helps. Altogether, this has a very significant effect on
    benchmarks that are bottlenecked by the checksum computation; our random
    test cases, for example, are so bottlenecked, and see order-of-magnitude
    performance improvements.

--- src/Codec/Compression/Zlib/Adler32.hs
+++ src/Codec/Compression/Zlib/Adler32.hs
@@ -1,30 +1,56 @@
+{-# LANGUAGE MagicHash #-}
 module Codec.Compression.Zlib.Adler32(
          AdlerState
        , initialAdlerState
        , advanceAdler
+       , advanceAdlerBlock
        , finalizeAdler
        )
  where
 
 import Data.Bits(shiftL, (.|.))
-import Data.Word(Word8, Word32)
+import qualified Data.ByteString.Lazy as L
+import GHC.Exts ( Word#, plusWord#, remWord# )
+import GHC.Word ( Word8(..), Word32(..) )
 
-data AdlerState = AdlerState { adlerA :: !Word32, adlerB :: !Word32 }
+data AdlerState = AdlerState { _adlerA :: Word#, _adlerB :: Word# }
 
 initialAdlerState :: AdlerState
-initialAdlerState = AdlerState 1 0
-
-adlerAdd :: Word32 -> Word32 -> Word32
-adlerAdd x y = (x + y) `mod` 65521
-{-# INLINE adlerAdd #-}
+initialAdlerState = AdlerState 1## 0##
 
 advanceAdler :: AdlerState -> Word8 -> AdlerState
-advanceAdler state b = AdlerState a' b'
+advanceAdler (AdlerState a b) (W8# v) = AdlerState a' b'
  where
-  a' = adlerAdd (adlerA state) (fromIntegral b)
-  b' = adlerAdd (adlerB state) a'
+  a' = (a `plusWord#` v) `remWord#` 65521##
+  b' = (b `plusWord#` a') `remWord#` 65521##
+{-# INLINE advanceAdler #-}
+
+advanceNoMod :: AdlerState -> Word8 -> AdlerState
+advanceNoMod (AdlerState a b) (W8# v) = AdlerState a' b'
+ where
+  a' = a `plusWord#` v
+  b' = b `plusWord#` a'
+{-# INLINE advanceNoMod #-}
+
+-- The block must be less than 5552 bytes long in this case
+advanceAdlerLimited :: AdlerState -> L.ByteString -> AdlerState
+advanceAdlerLimited !state !bl = AdlerState stateA' stateB'
+ where
+  !(AdlerState stateA stateB) = L.foldl' advanceNoMod state bl
+  stateA' = stateA `remWord#` 65521##
+  stateB' = stateB `remWord#` 65521##
+
+advanceAdlerBlock :: AdlerState -> L.ByteString -> AdlerState
+advanceAdlerBlock !state !bl
+  | L.length bl == 0 = state
+  | L.length bl == 1 = advanceAdler state (L.head bl)
+  | L.length bl < 5552 = advanceAdlerLimited state bl 
+  | otherwise = advanceAdlerBlock (advanceAdlerBlock state first5551) rest
+ where
+  (!first5551, !rest) = L.splitAt 5551 bl
 
 finalizeAdler :: AdlerState -> Word32
-finalizeAdler state = ((adlerB state) `shiftL` 16) .|.  adlerA state
-
-
+finalizeAdler (AdlerState a b) = high .|. low
+  where
+   high = (W32# b) `shiftL` 16
+   low = W32# a
